4d1326056482abefeb65771f146caf0184f1a64b
Moriarty178 <nguyenhaiphonganthi@gmail.com>
2024-09-03 16:54:24 +0700

    be pub -> admin web, save alert

--- src/main/java/com/example/GlobalTrackerGeo/Controller/DriverLocationController.java
+++ src/main/java/com/example/GlobalTrackerGeo/Controller/DriverLocationController.java
@@ -3,7 +3,7 @@ package com.example.GlobalTrackerGeo.Controller;
 import com.example.GlobalTrackerGeo.Dto.DriverLocationDTO;
 import com.example.GlobalTrackerGeo.Entity.Alert;
 import com.example.GlobalTrackerGeo.Entity.Driver;
-import com.example.GlobalTrackerGeo.Entity.DriverLocation;
+import com.example.GlobalTrackerGeo.Repository.AlertRepository;
 import com.example.GlobalTrackerGeo.Repository.DriverLocationRepository;
 import com.example.GlobalTrackerGeo.Repository.DriverRepository;
 import com.example.GlobalTrackerGeo.Service.DriverLocationService;
@@ -29,11 +29,13 @@ public class DriverLocationController {
     private DriverRepository driverRepository;
     @Autowired
     private DriverLocationRepository driverLocationRepository;
+    @Autowired
+    private AlertRepository alertRepository;
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/driver-location")
+    @MessageMapping("/driver-location")//tự động map tâất cả tin nhắn gửi từ Driver Web đến endpoint "/driver-location" để nó xử lý.
     public void handleDriverLocation(@RequestBody DriverLocationDTO location) {
         //Lưu thông tin vị trí vào PostgreSQL và Redis Stream
         driverLocationService.saveLocation(location);
@@ -42,19 +44,25 @@ public class DriverLocationController {
         messagingTemplate.convertAndSend("/topic/driver-location", location);
 
         //Phân tích và cảnh bảo ex: quá tốc độ, vào vùng cấm (GeoZone), vv...
-        if (location.getSpeed() > SPEED_LIMIT) {
+        if (location.getSpeed() > SPEED_LIMIT) {// sau này dùng 1 hàm riêng phân tích điều kiện gửi alert, ko chỉ quá tốc độ mà còn dựa trên khu vực địa lý vv...
             //gửi cảnh báo đến topic của tài xế cụ thể
-            messagingTemplate.convertAndSend("/topic/alert/" + location.getDriverId(), createdAlert(location));
+            messagingTemplate.convertAndSend("/topic/alert/" + location.getDriverId(), createdAndSaveAlert(location));
         }
 
     }
-    private Alert createdAlert(DriverLocationDTO location) {//tạo alert từ thông tin vị trị FE Driver Web (DriverLocationDTO) gửi sang
-        Alert alert = new Alert();
-        Driver driver = driverRepository.findById(location.getDriverId()).orElseThrow(() -> new IllegalArgumentException("Driver not found"));
 
+    //private boolean shouldSendAlert(DriverLocationDTO location) {...}
+
+    private Alert createdAndSaveAlert(DriverLocationDTO location) {//tạo alert từ thông tin vị trị FE Driver Web (DriverLocationDTO) gửi sang
+        Alert alert = new Alert();
+        Driver driver = driverRepository.findById(location.getDriverId())
+                .orElseThrow(() -> new IllegalArgumentException("Driver not found"));
         alert.setAlertType("Over Speed");
         alert.setDriver(driver);
         alert.setMessage("Yor are going over the speed limit.");
+
+        //Lưu alert vừa tạo vào csdl
+        alertRepository.save(alert);
         return alert;
     }
 }
